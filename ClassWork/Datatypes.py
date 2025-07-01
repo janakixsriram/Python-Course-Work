d3d7dae04f730634264145438a269d6b9634e4b7
janakixsriram <sriramsegu2003@gmail.com>
2025-07-01 12:55:11 +0530

    01-07-2025

---
+++ ClassWork/Datatypes.py
@@ -0,0 +1,62 @@
+#Numeric Types
+#int
+product_quantity = 5
+order_id = 29473
+print(product_quantity)
+
+#float
+
+product_price = 345.23
+discount_percentage = 13.3
+print(product_price) 
+
+#complex
+z = 5 + 2j
+print(z)
+
+#Sequence Types
+
+#string - str
+customer_name = "Rohit Sharma"
+delivery_address = "Bangalore , Karnataka"
+product = 'bat'
+print(customer_name)
+print(delivery_address)
+
+#list  mutable
+cart_items = ["Shoes","pants","T-shirts"]
+print(cart_items)
+
+#tuple immmutable
+warehouselocation = (21,34.234,341)
+print(warehouselocation)
+
+#Set Types mmutable
+available_sizes = {"s","m","l","xl"}
+print(available_sizes)
+
+#frozenset immutable
+set = frozenset(["sale","limited edition"])
+print(set)
+#MappingType
+#  dict - dictionary
+
+product_details = {
+    "name" : "Wireless Mouse",
+"brand": "Logitech",
+"price": 899.99,
+"rating": 4.5
+}
+customer_profile = {
+"name": "Anjali Verma",
+"email": "anjali@example.com",
+"prime_member": True
+}
+print(product_details)
+
+
+
+
+
+
+
--- ClassWork/variable.py
+++ ClassWork/variable.py
@@ -2,3 +2,4 @@ product_name = "Laptop"
 price = 45000
 in_stock = True
 print(product_name, price, in_stock)
+print (type(product_name))
