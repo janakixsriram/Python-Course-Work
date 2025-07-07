984211d9c75d82bb2ef14ab97bbaa7b3ee0179ef
janakixsriram <sriramsegu2003@gmail.com>
2025-07-07 19:43:49 +0530

    04-07-2025

---
+++ ClassWork/Dict.py
@@ -0,0 +1,42 @@
+student = {
+"name": "Alice",
+"age": 21,
+"course": "Computer Science"
+}
+print(student)
+
+#Dictionary Operations
+#Accessing Values
+
+print(student["name"]) # Output: Alice
+print(student.get("age")) # Output: 21
+
+#Adding and Updating Items
+student["age"] = 22 # Updating existing key
+student["city"] = "New York" # Adding a new key-value pair
+print(student)
+
+#Removing Items from a Dictionary
+#Using pop(key)
+age = student.pop("age")
+print(age) # Output: 22
+print(student)
+
+#Using del
+
+del student["course"]
+print(student)
+
+#Using popitem()
+
+student.popitem()
+print(student)
+
+#Using clear()
+
+student.clear()
+print(student) # Output: {}
+
+
+
+
