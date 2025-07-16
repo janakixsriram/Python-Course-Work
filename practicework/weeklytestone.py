8a20ecbb68d2b31d5f19e2327d4c071d994cf772
janakixsriram <sriramsegu2003@gmail.com>
2025-07-16 11:47:51 +0530

    16-07-2025

---
+++ practicework/weeklytestone.py
@@ -0,0 +1,12 @@
+date, month, year = input().split("-")
+print(f'{year}/{month}/{date}')
+ 
+ 
+sen = input().split()
+for i in sen:
+    print(i[::-1],end=' ')
+
+l = list(map(int,input().split()))
+while (0 in l):
+    l.remove(0)
+print(l)
