f0f6b5bd687eccab524e297fdc17a3c7875e2d1d
janakixsriram <sriramsegu2003@gmail.com>
2025-07-16 12:01:08 +0530

    16-07-2025

--- practicework/weeklytestone.py
+++ practicework/weeklytestone.py
@@ -10,3 +10,10 @@ l = list(map(int,input().split()))
 while (0 in l):
     l.remove(0)
 print(l)
+
+line = input().split()
+data = {}
+for i in line:
+    if i not in data and i!=' ':
+        data[i] = line.count(i)
+print(data)
