7c1cbc858425df69e71c479f1ac7d134958ee1ee
janakixsriram <sriramsegu2003@gmail.com>
2025-07-22 12:25:40 +0530

    18-07-2025

---
+++ ClassWork/grocery_store.py
@@ -0,0 +1,15 @@
+print("------ Welcome to Grocery Store ------")
+print("Here are the avaialble products:")
+
+super_store = {
+    1: {'product': 'Notebook', 'price': 45},
+    2:{'product': 'Pen',      'price': 10},
+    3:{'product': 'Pencil',   'price': 5},
+    4:{'product': 'Eraser',   'price': 7},
+    5:{'product': 'Marker',   'price': 20},
+    6:{'product': 'Sugar' ,   'price': 40},
+    7:{'product': 'Milk' ,    'price': 60},
+    8:{'product': 'Soap' ,    'price':25}
+}
+print(super_store)
+
