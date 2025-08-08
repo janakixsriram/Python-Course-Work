e9a8ea182d0cd8a60025038e739051e29158d36b
janakixsriram <sriramsegu2003@gmail.com>
2025-08-08 13:23:00 +0530

    07-07-2025

---
+++ practicework/for_loop.py/list_elements.py
@@ -0,0 +1,13 @@
+'''l = list(map(int,(input().split())))
+for i in range(len(l)):
+    if l[i]==3:
+        print("jai")
+'''
+def data():
+    global a
+    a=19
+    return a
+a=13
+b=1
+c= data()
+print( c)
