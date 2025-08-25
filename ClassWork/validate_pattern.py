2652ebde4e00ce510f73f7f489af462918831586
janakixsriram <sriramsegu2003@gmail.com>
2025-08-25 12:46:48 +0530

    25-08-2025

---
+++ ClassWork/validate_pattern.py
@@ -0,0 +1,9 @@
+#validating Name
+name = r'^[A-Za-z]{2-25} ( [A-Za-z] {2,25}) +$'
+#validating email
+email= r'^[A-Za-z0-9._]+@[a-zA-Z] +\. [a-zA-Z] {2,3}$'
+#validating phone number
+phoneno = r'^(?:\+91|0)?[6-9]\d{9}$'#->() is for pattern 
+#+91 98493452
+#0 843345234
+#93543254
